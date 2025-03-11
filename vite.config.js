513da7f8d9c50b90c78f5398cc2ca9320b28b3f4
Tao <zhoutao@hhbpay.com>
2025-03-11 15:03:36 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base:'idgenerate',
+  base:'prompt-tips',
   plugins: [vue()],
   resolve: {
     alias: {
